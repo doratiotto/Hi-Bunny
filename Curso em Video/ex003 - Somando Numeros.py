8288b8a6a3c9d53691aa0a55bb45880250b9076c
doratiotto <36274365+doratiotto@users.noreply.github.com>
2018-02-11 18:21:00 -0200

    Create ex003 - Somando Numeros.py

---
+++ Curso em Video/ex003 - Somando Numeros.py
@@ -0,0 +1,8 @@
+# Soma de dois numeros
+
+n1 = int(input('Give the first number'))
+n2 = int(input('Give the second number'))
+
+#print('the sum is {}' .format(n1 + n2))
+
+print('the sum of {} and {} is {}' .format(n1, n2, n1+n2))
