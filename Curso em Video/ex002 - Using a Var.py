a0ff0afb0c9bab7c67d166826d8c26411f94d37a
doratiotto <36274365+doratiotto@users.noreply.github.com>
2018-02-11 18:18:08 -0200

    Create ex002 - Using a Var.py

---
+++ Curso em Video/ex002 - Using a Var.py
@@ -0,0 +1,8 @@
+# Input .var e Print
+# v1
+user = input('hello, who are you?')
+print('welcome '+ user +'! we are happy to have you here!!')
+
+# v2
+user = input('hello, who are you again?')
+print('welcome {}! we are happy to have you here!!' .format(user))
